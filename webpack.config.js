2f57b3286849183ba37edf6f6ca94e74ab2be30c
amdev <56517440+thereisnodeveloper@users.noreply.github.com>
2024-07-13 16:37:16 -0400

    add file structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,16 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-  devServer: { watchFiles: [".src/**"] },
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: { static: "./dist" },
+  output: { filename: "main.js", path: path.resolve(__dirname, "dist") },
+//   optimization: { runtimeChunk: "single" },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.tml",
+    }),
+  ],
 };
